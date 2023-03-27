c6d838c6f821c3f0e6edfcb377528bdf3397c53d
Lizwi Silimela <lsilimela23@gmail.com>
2023-03-27 17:31:39 +0200

    User unit test

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Http\Response;
+use App\Models\User;
+
+class UserTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function test_show_user()
+    {
+        // Create a new user
+        $user = User::factory()->create();
+
+        // Call the show method on the user controller
+        $response = $this->get('/api/user/show/' . $user->id);
+
+        // Assert that the response has an HTTP status code of 200
+        $response->assertStatus(Response::HTTP_OK);
+
+        // Assert that the response has the correct user data
+        $response->assertJson([
+            'data' => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+            ]
+        ]);
+    }
+}
